3f82998e2bd929f9e874864710f5703cebd4bb5f
Choa0Kim <asjklcom@gmail.com>
2026-03-10 07:28:48 +0000

    Update yeonju BOJ solutions

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,35 +1,29 @@
-# + 오름차순..
-
-#입력
 N, M = map(int, input().split())
+path =[]
 used = [0]*(N+1)
-def recur(cnt, path, start):
-    # 끝점 설정
-    if cnt == M:
-        #최종 결과물
-        print(*path, sep=' ')
-        return
-    #반복 작업 설정
-    i = 1
-    for i in range(start, N + 1):
-        # 중복확인 조건 추가
-        if used[i] == 1:
-            continue # 다음 i로
-        
-        #오름차순 설정
-        # 만약 1번째로 뽑힌 숫자가 현재 뽑힌 숫자보다 크다면 다음으로 넘기기
-      
-        #중복이 아니라면, 숫자 추가하고
-        path.append(i)
-        # 사용기록 남기기
-        used[i] = 1
-        #다음 재귀에 넘겨줄 값 지정
-        # 오름차순:다음 숫자는 지금의 i보다 1 큰 숫자부터시작
-        recur(cnt+1, path, i+1)
-        #종료조건에 의해 재귀가 끝났을때, 다음 작업을 위한 조건설정
-        path.pop()# 다음 조합을 알아보기 위해 맨 뒤 숫자 제거
-        used[i]=0 #사용기록 초기화
 
-    
-# 시작점 설정
-recur(0, [], 1)
+#cnt: 카드 뽑은 횟수
+def recur(cnt, start):
+  # 종료조건
+  if cnt == M:
+    print(*path)
+    return
+  # N개 중에서 1개 뽑는 반복문
+  i = 1
+  for i in range(start, N+1):
+    # 사용여부 확인
+    if used[i] ==1:
+      continue
+    #사용여부 없으면, 경로에 추가하고
+    path.append(i)
+    #사용여부 기록
+    used[i] = 1
+    # 다음 카드 뽑기
+    recur(cnt+1, i+1)
+    # 다음 턴을 위해서 pop
+    path.pop()
+    # 사용여부 초기화
+    used[i] = 0
+
+
+recur(0, 1)    
