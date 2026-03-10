f326630495849ea5491989b3f6a371816d670e27
Choa0Kim <asjklcom@gmail.com>
2026-03-10 06:58:19 +0000

    Update yeonju BOJ solutions

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,27 +1,33 @@
-#입력
-N, M = map(int, input().split())
-used = [0]*(N+1)
-def recur(cnt, path):
-    # 끝점 설정
-    if cnt == M:
-        #최종 결과물
-        print(*path, sep=' ')
-        return
-    #반복 작업 설정
-    for i in range(1, N + 1):
-        # 중복확인 조건 추가
-        if used[i] == 1:
-            continue # 다음 i로
-        #중복이 아니라면, 숫자 추가하고
-        path.append(i)
-        # 사용기록 남기기
-        used[i] = 1
-        #다음 재귀에 넘겨줄 값 지정
-        recur(cnt+1, path)
-        #종료조건에 의해 재귀가 끝났을때, 다음 작업을 위한 조건설정
-        path.pop()# 다음 조합을 알아보기 위해 맨 뒤 숫자 제거
-        used[i]=0 #사용기록 초기화
+#import sys
+#sys.stdin = open("input.txt", "r")
+# 중복없이 수열 => 사용기록 남기기
 
+N, M = map(int, input().split())
+# print(N, M)
+# 경로 저장
+path = []
+used = [0]*(N+1)
+# cnt: 카드 뽑은 횟수
+def recur(cnt):
+  #종료 조건
+  if cnt == M :
+    print(*path)
+    return
+  # 1~N개의 선택지 중에서 1장 선택
+  for i in range(1, N+1):
+    #중복확인 
+    # 사용한 숫자라면
+    if used[i] ==1:
+      # 다음으로
+      continue
+    # 중복이 아니라면 숫자 추가하고
+    path.append(i)
+    # 사용기록 남기기
+    used[i] = 1
+    recur(cnt+1)
+    path.pop()
+    used[i]=0 # 사용기록 초기화(1,4 4,1은 가능하니께)
     
-# 시작점 설정
-recur(0, [])
+
+recur(0)
+
